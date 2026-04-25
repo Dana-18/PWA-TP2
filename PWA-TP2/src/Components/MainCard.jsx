cba1a3ebceec9f4f83b6aa00e33aa5109cdfb1ba
Dana-18 <123900753+Dana-18@users.noreply.github.com>
2026-04-25 00:05:34 -0300

    Ya quedo hecho el sidebar

--- PWA-TP2/src/Components/MainCard.jsx
+++ PWA-TP2/src/Components/MainCard.jsx
@@ -1,30 +1,27 @@
 import Title from "./Title";
-import Badge from "./Badge";    
+import Badge from "./Badge";
 import RoutineTracker from "./RoutineTracker";
 import Button from "./Button";
 import {SquarePen, ArrowRight} from "lucide-react";
 
 export default function MainCard () {
     return (
-        <>
-            <div className="flex items-start flex-col w-full bg-white h- rounded-3xl mt-12 p-6">
-                <div className="grid grid-cols-4 items-start w-full">
-                    <Title className="col-span-3" titulo="Rutina 1" descripcion="Descripci√≥n de la rutina 1." size="medium" >
-                        <Badge className="ml-3 bg-[#71F8E4]" text="ACTIVA" />
-                    </Title>
-                    <SquarePen className="col-span-1 ml-auto text-[#10B981]" size={24} />
-                </div>
-                <div className="mt-4 w-full">
-                    <span className="tracking-wide text-stone-500">ESTA SEMANA</span>
-                    <RoutineTracker className="mt-2" /> 
-                </div>
-                <div className="w-full flex">
-                    {/* Ac√° hay que pasar el prop "to: ruta" para que vaya a la vista de comenzar la sesion,  */}
-                    <Button text="Comenzar Sesi√≥n" className="mt-4 ml-auto flex items-center gap-1">
-                        <ArrowRight size={20} />
-                    </Button>
-                </div>
+        <div className="flex items-start flex-col w-full rounded-3xl bg-white p-6 shadow-sm mt-12">
+            <div className="grid grid-cols-4 gap-4 items-start w-full">
+                <Title className="col-span-3" titulo="Rutina 1" descripcion="DescripciÛn de la rutina 1." size="medium" >
+                    <Badge className="ml-3 bg-[#71F8E4]" text="ACTIVA" />
+                </Title>
+                <SquarePen className="col-span-1 ml-auto text-[#10B981]" size={24} />
             </div>
-        </>
+            <div className="mt-4 w-full">
+                <span className="tracking-wide text-stone-500">ESTA SEMANA</span>
+                <RoutineTracker className="mt-2" />
+            </div>
+            <div className="w-full flex">
+                <Button text="Comenzar SesiÛn" className="mt-4 ml-auto flex items-center gap-1">
+                    <ArrowRight size={20} />
+                </Button>
+            </div>
+        </div>
     );
 }
--- PWA-TP2/src/Components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-export default function Navbar({className}){
-    return (
-        <>
-            <nav className={`bg-[#d4ceb6] ${className}`}>
-                
-            </nav>
-        </>
-    );
-}
