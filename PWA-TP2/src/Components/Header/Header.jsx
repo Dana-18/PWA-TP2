d1c08fc3eaad2c2a114d4644789d4156a872f6d2
Tomas Mengon <123125239+Tommas138@users.noreply.github.com>
2026-05-06 16:13:53 -0300

    Update Header.jsx

--- PWA-TP2/src/Components/Header/Header.jsx
+++ PWA-TP2/src/Components/Header/Header.jsx
@@ -8,7 +8,7 @@ import React from "react";
 export default function Header() {
 
     return (
-        <header className="bg-white h-14 sm:h-16 md:h-28 w-full sticky top-0 z-50 p-3 sm:p-4 flex items-center gap-4 border-b border-gray-300">
+        <header className="bg-white h-14 sm:h-16 md:h-28 w-full sticky top-0 z-50 p-3 sm:p-4 flex items-center gap-4 border-none">
                 <div className="ml-auto gap-2 sm:gap-4">
                     <LanguageSwitcher />
                 </div>
